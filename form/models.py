840cddd284dbcd4e18d10e3bb30d4b774129847d
alexander <a.samoyl11@gmail.com>
2019-11-11 03:12:58 +0300

    send_message

--- form/models.py
+++ form/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-import smtplib, ssl
+from django.core.mail import send_mail
 
 class Form(models.Model):
     firstName = models.CharField(max_length=120)
@@ -8,19 +8,8 @@ class Form(models.Model):
     completed = models.BooleanField(default=False)
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
-        port = 465  # For SSL
-        smtp_server = "smtp.gmail.com"
-        sender_email = "test4sendingresults@gmail.com"  # Enter your address
-        receiver_email = "test4sendingresults@gmail.com"  # Enter receiver address
-        password = '9250087828'
-        message = """\
-        Subject: Hi there
 
-        This message is sent from Python."""
+        send_mail('hello', 'wazap', settings.EMAIL_HOST_USER, [settings.EMAIL_HOST_USER])
 
-        context = ssl.create_default_context()
-        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-            server.login(sender_email, password)
-            server.sendmail(sender_email, receiver_email, message)
     def _str_(self):
         return self.firstName
