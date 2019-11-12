7ed69de089eb530ee9d9fb10303adbe89d51b474
alexander <a.samoyl11@gmail.com>
2019-11-12 23:34:13 +0300

    telebot

--- form/models.py
+++ form/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 from django.core.mail import send_mail
+import telebot
+
+token = '1040941161:AAGRKhQAsHaPsXVQH6nfuaE_Sap-9qnN878'
+
+from telebot import types
+
+bot= telebot.TeleBot(token)
 
 class Form(models.Model):
     firstName = models.CharField(max_length=120)
@@ -11,5 +18,8 @@ class Form(models.Model):
 
         send_mail('New Client', f'Телефон: {self.telephone}\n Имя: {self.firstName}\n Фамилия: {self.lastName}\n', 'alquilar.cochee@gmail.com', ['alquilar.cochee@gmail.com'])
 
+        bot.send_message(chat_id='270129913', text="Новый клиент: имя: {} {}, телефон: {}".format(self.firstName, self.lastName, self.telephone))
+        bot.send_message(chat_id='251780463', text="Новый клиент: имя: {} {}, телефон: {}".format(self.firstName, self.lastName, self.telephone))
+        bot.send_message(chat_id='309350089', text="Новый клиент: имя: {} {}, телефон: {}".format(self.firstName, self.lastName, self.telephone))
     def _str_(self):
         return self.firstName
