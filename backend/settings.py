cc91faa1ae53d56adf3fab0401b99583f1276793
alexander <a.samoyl11@gmail.com>
2019-11-12 10:20:31 +0300

    fix

--- backend/settings.py
+++ backend/settings.py
@@ -125,7 +125,7 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'alquilar.cochee@gmail.com'
 EMAIL_HOST_PASSWORD = 'nikitaboss228'
 EMAIL_USE_TLS = True
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' #uncomment when use locally
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
