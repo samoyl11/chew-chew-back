0c2a12697656e88342d5cf1aa3518926e6ba200e
alexander <a.samoyl11@gmail.com>
2019-11-13 11:24:43 +0300

    char

--- form/models.py
+++ form/models.py
@@ -21,5 +21,6 @@ class Form(models.Model):
         bot.send_message(chat_id='270129913', text="Новый клиент: имя: {} {}, телефон: {}".format(self.firstName, self.lastName, self.telephone))
         bot.send_message(chat_id='251780463', text="Новый клиент: имя: {} {}, телефон: {}".format(self.firstName, self.lastName, self.telephone))
         bot.send_message(chat_id='309350089', text="Новый клиент: имя: {} {}, телефон: {}".format(self.firstName, self.lastName, self.telephone))
+        bot.send_message(chat_id='-368201757', text="Новый клиент: имя: {} {}, телефон: {}".format(self.firstName, self.lastName, self.telephone))
     def _str_(self):
         return self.firstName
